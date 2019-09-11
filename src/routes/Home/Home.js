e1528abad20cf2f38fed7bc328cbb498b5d8676a
Jordan Epps <jordanepps@gmail.com>
2019-09-11 13:49:23 -0400

    Create and style top section for home on mobile

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Home.css';
-import home from '../../img/home.jpg';
 
 function Home() {
   return (
     <div className="home-container">
       <div className="top-section">
-        <img src={home} />
         <h1>
           Things happen. <br /> We can fix it.
         </h1>
