064dc8e0ca54b23522854e1f7585c532649ab9e4
Jordan Epps <jordanepps@gmail.com>
2019-09-14 15:31:46 -0400

    Add functions to handle login errors and log auth token

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PageContext } from '../../context/Context';
+import AuthApiService from '../../services/auth-api-service';
 
 function Login({ history }) {
   //   console.log(history.location.pathname);
   const [page, setCurrentPage] = useContext(PageContext);
-  //   console.log(page);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     function setPage() {
@@ -14,16 +15,60 @@ function Login({ history }) {
     setPage(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = e.target;
+
+    AuthApiService.postLogin({
+      email: email.value,
+      password: password.value
+    })
+      .then(res => {
+        // email.value = password.value = '';
+        console.log(res);
+      })
+      .catch(handleError);
+  }
+
+  function handleError(res) {
+    setError(res.error);
+  }
+
+  function renderError() {
+    return (
+      <div>
+        <h3>{error}</h3>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        {!error ? '' : renderError()}
         <div>
-          <label>Email:</label>
-          <input />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="email"
+            required
+          />
         </div>
         <div>
-          <label>Password:</label>
-          <input />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="new-password"
+            required
+          />
+        </div>
+
+        <div>
+          <button>Login</button>
         </div>
       </form>
     </div>
