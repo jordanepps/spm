738be163e707fa28837e0275deb636830fe2cec5
Jordan Epps <jordanepps@gmail.com>
2019-09-14 11:12:38 -0400

    Handle password match check for registration

--- src/routes/Registration/Registration.js
+++ src/routes/Registration/Registration.js
@@ -1,8 +1,10 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PageContext } from '../../context/Context';
 
 function Registration() {
   const [page, setCurrentPage] = useContext(PageContext);
+  const [error, setError] = useState(null);
+  const [passwordMatch, setPasswordMatch] = useState(null);
 
   useEffect(() => {
     function setPage() {
@@ -12,20 +14,64 @@ function Registration() {
     setPage(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { email, password, verify_password } = e.target;
+
+    if (password.value !== verify_password)
+      return setPasswordMatch('Password does not match');
+  }
+
+  function resetPasswordMatch() {
+    setPasswordMatch(null);
+  }
+
+  function renderPasswordError() {
+    return (
+      <div>
+        <h3>Passwords do not match</h3>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label>Email:</label>
-          <input />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            autoComplete="email"
+            required
+          />
+        </div>
+        {!passwordMatch ? '' : renderPasswordError()}
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="new-password"
+            required
+            onChange={resetPasswordMatch}
+          />
         </div>
         <div>
-          <label>Password:</label>
-          <input />
+          <label htmlFor="verify_password">Verify Password:</label>
+          <input
+            type="password"
+            name="verify_password"
+            id="verify_password"
+            autoComplete="new-password"
+            required
+            onChange={resetPasswordMatch}
+          />
         </div>
         <div>
-          <label>Verify Password:</label>
-          <input />
+          <button>Register</button>
         </div>
       </form>
     </div>
