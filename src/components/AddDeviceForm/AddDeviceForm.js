3f22265293f00168f5fe35c3cf0df19df933a851
Jordan Epps <jordanepps@gmail.com>
2019-09-18 10:08:12 -0400

    Add button and modal for new devices

---
+++ src/components/AddDeviceForm/AddDeviceForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './AddDeviceForm.css';
+
+function AddDeviceForm() {
+  return <form>Form</form>;
+}
+
+export default AddDeviceForm;
--- src/routes/Inventory/Inventory.js
+++ src/routes/Inventory/Inventory.js
@@ -1,10 +1,24 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Modal from 'react-modal';
 import { PageContext } from '../../context/Context';
+import AddDeviceForm from '../../components/AddDeviceForm/AddDeviceForm';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root');
 
 function Inventory() {
-  //   console.log(history.location.pathname);
   const [page, setCurrentPage] = useContext(PageContext);
-  //   console.log(page);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     function setPage() {
@@ -14,7 +28,33 @@ function Inventory() {
     setPage(); // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
-  return <div>Inventory Page</div>;
+  function handleAddDeviceClick() {
+    openModal();
+  }
+
+  function openModal() {
+    setModalOpen(true);
+  }
+
+  function closeModal() {
+    setModalOpen(false);
+  }
+
+  return (
+    <div>
+      <h1>Device Inventory</h1>
+      <button onClick={handleAddDeviceClick}>Add Device</button>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Device"
+        closeTimeoutMS={2000}
+      >
+        <AddDeviceForm />
+      </Modal>
+    </div>
+  );
 }
 
 export default Inventory;
